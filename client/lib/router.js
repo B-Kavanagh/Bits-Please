5f09f31ff0fcc856fee83119b2dac2965abb14e0
Spudzer150 <murphyberry@hotmail.co.uk>
2017-03-30 14:51:06 +0100

    Bren's main page

--- client/lib/router.js
+++ client/lib/router.js
@@ -1,3 +1,4 @@
+
 Router.route('/aboutUs');
 Router.route('/findUs');
 Router.route('/dataTable');
@@ -8,6 +9,16 @@ Router.route('/', {
 Router.route('/reviews');
 Router.route('/community');
 Router.route('/hardware');
-Router.route('/top');
 Router.route('/walkthroughs');
+Router.route('/PC');
+Router.route('/PS4');
+Router.route('/Xbox');
 Router.route('/more');
+Router.route('/Witcher3');
+Router.route('/MirrorsEdge');
+Router.route('/StarWars');
+Router.route('/Honour');
+Router.route('/Skyrim');
+Router.route('/Rocketleague');
+Router.route('/Zelda');
+Router.route('/Darksouls');
